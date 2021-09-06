1708c2ea767945b3c0c730408c2209f6ab934ef9
modem7 <modem7@gmail.com>
2021-09-06 22:57:55 +0100

    Update entry.sh

--- test/entry.sh
+++ test/entry.sh
@@ -3,5 +3,7 @@
 URL=https://share.modem7.com/nEsO3/fEduWuXa02.mp4
 FILEURL=$URL
 # Download Video
+echo Downloading $URL
+echo Downloading $FILEURL
 curl $URL --output /usr/share/nginx/html/rickroll.mp4
 exec "$@"
